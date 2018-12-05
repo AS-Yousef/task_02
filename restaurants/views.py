fe58bdfdad0ed2e349a721ecf972ebc1384711ae
AS-Yousef <a.s.sultan@hotmail.com>
2018-12-05 13:20:41 +0300

    task_02 submission

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def restaurant(request):
+	context={
+	"msg":"Hello World!",
+	}
+	return render (request, 'resturant.html', context)
